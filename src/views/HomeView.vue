5bdde2e3e51849721658d276db285cc3d28a9c61
Ziyaad <rhyman.ziyaad@gmail.com>
2022-07-27 16:27:27 +0400

    [+][UPD] Handle chart

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,74 +1,119 @@
 <template>
-  <main>
-    <v-stage :config="configKonva">
+  <div>
+    <v-stage ref="stage" :config="configKonva">
       <v-layer>
-        <v-rect
-          v-for="rectangle in rectangles"
-          :key="rectangle.name"
-          :config="rectangle"
-        />
         <v-line
-          v-for="item in 10"
-          :key="item"
+          v-for="hl in hlines"
+          :key="hl.id"
           :config="{
-            points: [0, Math.round(item * 30), width, Math.round(item * 30)],
-            stroke: 'red',
-            strokeWidth: 0.5,
+            id: hl.id,
+            points: hl.points,
+            stroke: 'lightgrey',
+            strokeWidth: 0.3,
           }"
-        />
+        ></v-line>
+        <v-text
+          v-for="t1 in yaxis"
+          :key="t1.id"
+          :config="{
+            id: t1.id,
+            x: t1.labelX,
+            y: t1.labelY,
+            text: t1.labelText,
+            fontSize: 10,
+            fontFamily: 'Calibri',
+            fill: 'black',
+          }"
+        ></v-text>
+        <v-group>
+          <v-rect
+            :config="{
+              x: 40,
+              y: 20,
+              width: 50,
+              height: blockSnapSize * input1,
+              fill: '#4472c4',
+              shadowBlur: 2,
+            }"
+          />
+          <v-rect
+            :config="{
+              x: 120,
+              y: total - input2 * blockSnapSize,
+              width: 50,
+              height: blockSnapSize * input2,
+              fill: '#4472c4',
+              shadowBlur: 2,
+            }"
+          />
+          <v-rect
+            :config="{
+              x: 200,
+              y: total - input3 * blockSnapSize,
+              width: 50,
+              height: blockSnapSize * input3,
+              fill: '#4472c4',
+              shadowBlur: 2,
+            }"
+          />
+        </v-group>
       </v-layer>
     </v-stage>
-    <ul>
-      <li v-for="index in 10" :key="index">{{ index }}</li>
-    </ul>
-  </main>
+  </div>
 </template>
 
 <script>
+const width = 300;
+const height = 270;
 export default {
   data() {
     return {
+      list: [],
       configKonva: {
-        width: 200,
-        height: 300,
+        width: width,
+        height: height,
       },
-      rectangles: [
-        {
-          rotation: 0,
-          x: 10,
-          y: 10,
-          width: 100,
-          height: 100,
-          scaleX: 1,
-          scaleY: 1,
-          fill: "red",
-          name: "rect1",
-        },
-        {
-          rotation: 0,
-          x: 150,
-          y: 150,
-          width: 100,
-          height: 100,
-          scaleX: 1,
-          scaleY: 1,
-          fill: "green",
-          name: "rect2",
-        },
-        {
-          rotation: 0,
-          x: 80,
-          y: 80,
-          width: 100,
-          height: 100,
-          scaleX: 1,
-          scaleY: 1,
-          fill: "blue",
-          name: "rect3",
-        },
-      ],
+      input1: 10,
+      input2: 6,
+      input3: 1,
+      blockSnapSize: 20,
+      vlines: [],
+      hlines: [],
+      yaxis: [],
+      x: 0,
+      y: 0,
+      charts: [],
+      labelY: 0,
+      labelX: 0,
+      highestValue: 10,
+      lowestValue: -2,
+      startingValue: 0,
+      total: 0,
     };
   },
-  mounted() {},
+  computed() {},
+  methods: {},
+  mounted() {
+    let padding = this.blockSnapSize;
+    let labelPosition = 0;
+    let label = this.highestValue + 1;
+    this.total = this.highestValue * this.blockSnapSize + this.blockSnapSize;
+
+    for (var j = 0; j < height / padding; j++) {
+      this.hlines.push({
+        id: Math.round(Math.random() * 999).toString(),
+        points: [0, Math.round(j * padding), width, Math.round(j * padding)],
+      });
+    }
+
+    for (var j = 0; j < height / padding; j++) {
+      this.yaxis.push({
+        id: Math.round(Math.random() * 999).toString(),
+        labelX: 0,
+        labelY: (labelPosition += padding),
+        labelText: (label -= 1),
+      });
+    }
+  },
 };
 </script>
